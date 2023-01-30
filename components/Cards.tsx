7305ad407fa60d25020541ebfd1e0b081f548721
j3rryl <jeremymunroe0@gmail.com>
2023-01-30 08:41:38 +0300

    indexes

---
+++ components/Cards.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className=' flex justify-center items-center'>
+        <div className=' w-1/3 h-2/5 !bg-black'>
+            <h2>Apply for loans</h2>
+        </div>
+        <div className=' w-1/3 h-2/5'>
+            <h2>Help others</h2>
+
+        </div>
+        <div className=' w-1/3 h-2/5'>
+            <h2>Be part of the community.</h2>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Cards
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import loan1 from '../images/loan1.jpg'
+import Cards from '../components/Cards'
 
 export default function Home() {
   return (
     <div className='w-full h-full'>
       <div className={`${styles.container} ${styles.container1}`}>
-
+        
+        {/* <Cards/> */}
       </div>
       <div className={`${styles.container} ${styles.container2}`}>
+        <h2>Hello</h2>
 
       </div>
       {/* <div className='h-screen'>

e73251c74501a1c230f62c468131391588cc693c
j3rryl <jeremymunroe0@gmail.com>
2023-01-30 16:55:25 +0300

    gloabal ts coming soon

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import NextAuth from 'next-auth/next'
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import clientPromise from '../../../lib/mongodb'
 
 
 
@@ -12,11 +14,18 @@ export default NextAuth({
       // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
       // @ts-ignore
       scope: "read:user",
+      
     }),
     
     // GoogleProvider({
     //   clientId: '',
     //   clientSecret: '',
     // }),
-    ]
+    ],
+    adapter:MongoDBAdapter(clientPromise),
+    session: {
+    },
+    jwt: {
+      secret: process.env.AUTH_SECRET,
+    },
 })

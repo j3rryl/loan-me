1b7058a19da2bcdba849823d57b8accd516e0791
j3rryl <jeremymunroe0@gmail.com>
2023-01-31 19:17:40 +0300

    login done

--- pages/authentication/login.tsx
+++ pages/authentication/login.tsx
@@ -1,13 +1,16 @@
 import { getProviders, signIn } from 'next-auth/react'
+import Image from 'next/image';
+import gitIcon from '../../icons/github-mark-white.png'
 import React, { ReactElement } from 'react'
 import styles from '../../styles/Form.module.css'
+import Link from 'next/link';
 const Login = ({providers}:any) => {
   const ProvidersButtons = ({ providers }: any) => (
     <div className='flex justify-evenly'>
       {Object.values(providers).map(
         (provider: any) =>
           provider.name !== "Credentials" && (
-            <button className={styles.authbuttons}
+            <button className={`${provider.name=="Google"?"!bg-white !text-black":""} ${styles.authbuttons} flex justify-center`}
               key={provider.name}
               onClick={()=>{
                 signIn(provider.id,{
@@ -15,6 +18,10 @@ const Login = ({providers}:any) => {
                 })
               }}
               >
+                {provider.name=="Google"?
+                <img className={styles.google} src="https://developers.google.com/identity/images/g-logo.png" height='30' width='30'/>:
+                <Image placeholder='blur' src={gitIcon} className={styles.github} alt='git-icon' height='30' width='30'/>
+              }
                 Sign in with {provider.name}
             </button>
           )
@@ -23,7 +30,7 @@ const Login = ({providers}:any) => {
   );
   return (
     <div className={styles.formcontroller}>
-      <h2 className='mt-16 mb-20'>Login Form</h2>
+      <h2 className='mt-16 mb-20 font-black text-3xl'>Login Form</h2>
        <form className={styles.formcontrol}>
         <label htmlFor='email'>Email</label><br />
         <input name='email' type='email' placeholder='Enter Email' className={styles.inputcontrol}/><br />
@@ -33,9 +40,21 @@ const Login = ({providers}:any) => {
         className={styles.inputcontrol}/><br/>
         <input className={styles.submit}
          type='submit' 
+        //  onClick={()=>{
+          // signIn('github')
+        // }}
+         name='login' value='Login' /><br/>
+         <Link href='/authentication/register'
+         className='!my-12'>Don't have an account? Register here
+         </Link>
+         <div className='my-4 mx-2 flex justify-between text-center '> 
+         <hr className= {`${styles.line} `}/>
+         <p>OR</p>
+         <hr className={`${styles.line} `}/>
+         </div>
          
-         name='login' value='Login' />
-         <ProvidersButtons providers={providers}/>
+         <ProvidersButtons providers={providers} />
+         
        </form>
        
     </div>
--- pages/authentication/register.tsx
+++ pages/authentication/register.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React, { ReactElement } from 'react'
+import { getProviders, signIn } from 'next-auth/react'
+import Image from 'next/image';
+import gitIcon from '../../icons/github-mark-white.png'
+import styles from '../../styles/Form.module.css'
+import Link from 'next/link';
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={styles.formcontroller}>
+      <h2 className='mt-16 mb-20 font-black text-3xl'>Login Form</h2>
+       <form className={styles.formcontrol}>
+        <label htmlFor='first_name'>First Name</label><br />
+        <input name='first_name' type='text' placeholder='Enter First Name' className={styles.inputcontrol}/><br />
+        <label htmlFor='last_name'>Last Name</label><br />
+        <input name='last_name' type='text' placeholder='Enter Last Name' className={styles.inputcontrol}/><br />
+        <label htmlFor='email'>Email</label><br />
+        <input name='email' type='email' placeholder='Enter Email' className={styles.inputcontrol}/><br />
+        <label htmlFor='password'>Password</label><br />
+        <input name='password' type='password' placeholder='Enter Password' 
+        autoComplete='off'
+        className={styles.inputcontrol}/><br/>
+        <input className={styles.submit}
+         type='submit' 
+        //  onClick={()=>{
+          // signIn('github')
+        // }}
+         name='register' value='Register' /><br/>
+         <Link href='/authentication/login'
+         className='!my-12'>Have an account? Login here
+         </Link>       
+       </form>
+       
+    </div>
   )
 }
 
 export default Register
+Register.getLayout = function PageLayout(page: ReactElement){
+  return (
+      <>
+      {page}
+      </>
+  )
+}
